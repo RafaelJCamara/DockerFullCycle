42ff4647c695015059970130ef185894ee58756b
RafaelJCamara <rafaelcamarac@gmail.com>
2021-12-30 19:21:33 +0000

    Add node and mysql containers and respective communication.

---
+++ Docker-compose/node/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mysql = require('mysql');
+
+const config = {
+    host: 'db', //docker container to connect with
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const connection = mysql.createConnection(config);
+const sql = `INSERT INTO people(name) values('Rafael')`;
+connection.query(sql);
+connection.end();
+
+app.get("/:name", (req,res)=>{
+    /*
+    const {name} = req.params;
+    const sql = `INSERT INTO people(name) values('${name}')`;
+    connection.query(sql);
+    connection.end();
+    */
+    res.send("<h1>Full Cycle!!!</h1>")
+});
+
+app.listen(port, ()=>{
+    console.log(`Server listening in port ${port}`)
+});
